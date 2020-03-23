e1ae89abf411d908ddd184149892af32bf57e986
AmbroiseMorandeau <62509360+AmbroiseMorandeau@users.noreply.github.com>
2020-03-23 17:58:06 +0100

    Delete header.php

--- projet-PHP-cookies/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Système de panier pour site e-commerce</title>
-</head>
-<body>
